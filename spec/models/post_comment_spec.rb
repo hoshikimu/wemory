80fd7f43d43bd6350930a8770e99bfe55c059324
vagrant <vagrant@localhost.localdomain>
2020-09-07 07:04:57 +0000

    Add test for order_model

--- spec/models/post_comment_spec.rb
+++ spec/models/post_comment_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe PostComment, type: :model do
   describe 'バリデーションテスト' do
-    let(:user) { create(:user)}
+    let(:user) { create(:user) }
     let(:post_image) { create(:post_image, user_id: user.id ) }
     let!(:post_comment) { build(:post_comment, user_id: user.id, post_image_id: post_image.id) }
     subject { post_comment.valid? }
