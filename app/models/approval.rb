81cdbae5f23c047e2bdf13e7d77bd254339b6ecd
vagrant <vagrant@localhost.localdomain>
2020-09-11 05:44:45 +0000

    Edit notification function for approvals

--- app/models/approval.rb
+++ app/models/approval.rb
@@ -1,24 +1,27 @@
 class Approval < ApplicationRecord
   belongs_to :approver, class_name: "User"
   belongs_to :approvered, class_name: "User"
+  has_many :notifications, dependent: :destroy
   validates :permission_status, presence: true
 
   enum permission_status: {"閲覧者(アルバム注文不可)" => 0, "閲覧者(アルバム注文可)" => 1}
 
-  def create_notification_approve(current_user, approvered_id)
+  def create_notification_approve(current_user)
     temp = Notification.where(visitor_id: current_user.id, visited_id: approvered_id, action: "approve")
     if temp.blank?
       notification = current_user.active_notifications.new(
       visited_id: approvered_id,
+      permission_status: permission_status,
       action: "approve"
       )
       notification.save
     end
   end
 
-  def create_notification_edit_approve(current_user, approvered_id)
+  def create_notification_edit_approve(current_user)
     notification = current_user.active_notifications.new(
     visited_id: approvered_id,
+    permission_status: permission_status,
     action: "edit_approve"
     )
     notification.save
