0c80060ae73934c5ac5016ecc9aad585a3421c1a
vagrant <vagrant@localhost.localdomain>
2020-09-22 11:12:05 +0000

    Add devise views for admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'homes#about'
   devise_for :users
+  devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :post_images do
