708f22c3d7c9a58dcb0535d2eba0103f910e1e75
自åhoshikimu <h.k.fir.m.ice.hs@gmail.com>
2020-09-26 15:12:04 +0000

    Edit authentication for devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  # before_action :authenticate_user!, except: [:about, :contact, :contact_create]
+  before_action :authenticate_user!, except: [:about, :contact, :contact_create]
+  before_action :authenticate_admin!
 
   def after_sign_up_path_for(resource)
     case resource
